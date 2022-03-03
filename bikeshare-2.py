6f8b376df0385985655a976ba942c3bba3597c80
luckyrobot2000 <shanshanbingbing@163.com>
2022-03-03 13:46:21 +0800

    Refactor main function

--- bikeshare-2.py
+++ bikeshare-2.py
@@ -224,8 +224,6 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         display_data(df)
-
-
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
